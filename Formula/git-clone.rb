88c0c7a3508ab551b85741ae7aef01e9be7e844a
flare576 <flare576@gmail.com>
2021-06-17 15:33:04 -0500

    Updating checksum

--- Formula/git-clone.rb
+++ Formula/git-clone.rb
@@ -2,7 +2,7 @@ class GitClone < Formula
   desc "Tool for maintaining multiple git accounts across any git system supporting HTTPS."
   homepage "https://github.com/Flare576/git-clone"
   url "https://github.com/Flare576/git-clone/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 ""
+  sha256 "ee99d3fb78f8c1d6980b2b3c8f913521bac71dbb56fa041bc888fe913c03e103"
   license "MIT"
 
   depends_on "git"
