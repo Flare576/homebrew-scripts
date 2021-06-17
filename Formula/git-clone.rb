0d012de3a9051ffca00eb1f0414b70f85620244e
flare576 <flare576@gmail.com>
2021-06-17 15:06:42 -0500

    Removing run thing and bash

--- Formula/git-clone.rb
+++ Formula/git-clone.rb
@@ -1,15 +1,14 @@
-class git_clone < Formula
+class GitClone < Formula
   desc "Tool for maintaining multiple git accounts across any git system supporting HTTPS."
   homepage "https://github.com/Flare576/git-clone"
   url "https://github.com/Flare576/git-clone/archive/refs/tags/v0.0.4.tar.gz"
   sha256 "099915c261c2da9857f9e41f970e7678afc5734f5e749f4336a5ecde923df7ee"
   license "MIT"
 
-  depends_on "git" => !run
-  depends_on "bash" => !run
+  depends_on "git"
 
   def install
-    bin.install "bin/git-clone.sh" => "clone"
+    bin.install "git-clone.sh" => "clone"
   end
 
   test do
