3f57b035e5bd659ad09bfb30d98432f6cd8d44f9
flare576 <flare576@gmail.com>
2021-06-18 11:12:32 -0500

    Adding hash

--- Formula/gac.rb
+++ Formula/gac.rb
@@ -2,7 +2,7 @@ class Gac < Formula
   desc "Git Add Commit makes common git tasks easier"
   homepage "https://github.com/Flare576/gac"
   url "https://github.com/Flare576/gac/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 ""
+  sha256 "69b737262772788f5e46eefac6a02f06e455d76fe2f9ea67b898243d20aca28c"
   license "MIT"
 
   depends_on "git"
