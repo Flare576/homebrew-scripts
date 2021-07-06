02ecebbe57746445bd20767c47939e64e6bb98c1
flare576 <flare576@gmail.com>
2021-07-06 17:07:54 -0500

    chore(newScript): version bump

--- Formula/newScript.rb
+++ Formula/newScript.rb
@@ -1,8 +1,8 @@
 class Newscript < Formula
   desc "Quickly generate script stubs in js, sh, or py"
   homepage "https://github.com/Flare576/newScript"
-  url "https://github.com/Flare576/newScript/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "6e8fb4cfa6d2d6c5eb825d227a8ba7b737b296dd676e0e43791141b16f6c4aa0"
+  url "https://github.com/Flare576/newScript/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "8bf9e7bbc2595a54b6acf85f8a3fa185fa755b59e4afcbf23252272af3a807c4"
   license "MIT"
 
   depends_on "node"
