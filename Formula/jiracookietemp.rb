75143d7dac28a940e64f28a9995083c194af6ed0
flare576 <flare576@gmail.com>
2021-08-13 10:54:23 -0500

    rename

--- Formula/jiracookietemp.rb
+++ Formula/jiracookietemp.rb
@@ -1,6 +1,6 @@
 class Jiracookie < Formula
   desc "Allows using cookies with go-jira for hosted instances"
-  homepage "https://github.com/Flare576/jiraCookie"
+  homepage "https://github.com/Flare576/jiracookie"
   url "https://github.com/Flare576/jiraCookie/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "1ab7e7b84b6d664bf37ca8bac52976d226e59d005dfc82c2ebb970f1324ff9e2"
   license "MIT"
@@ -8,7 +8,7 @@ class Jiracookie < Formula
   depends_on "node"
 
   def install
-    bin.install "bin/jiraCookie"
+    bin.install "bin/jiracookie"
   end
 
   test do
