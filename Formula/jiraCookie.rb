c6cf6d3961a7f37184bca435311ddef2db2a78b8
flare576 <flare576@gmail.com>
2021-08-13 10:01:25 -0500

    feat(jiraCookie): add recipe

---
+++ Formula/jiraCookie.rb
@@ -0,0 +1,17 @@
+class Jiracookie < Formula
+  desc "Allows using cookies with go-jira for hosted instances"
+  homepage "https://github.com/Flare576/jiraCookie"
+  url "https://github.com/Flare576/jiraCookie/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "1ab7e7b84b6d664bf37ca8bac52976d226e59d005dfc82c2ebb970f1324ff9e2"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    bin.install "bin/jiraCookie"
+  end
+
+  test do
+    system "echo", "temp value for formula validation"
+  end
+end
