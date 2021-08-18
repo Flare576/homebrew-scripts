32a5ebb328e1c42f8813ae03284103e1a0ddaf66
flare576 <flare576@gmail.com>
2021-08-18 12:29:07 -0500

    fix(vroom): Bump

--- Formula/vroom.rb
+++ Formula/vroom.rb
@@ -1,8 +1,8 @@
 class Vroom < Formula
   desc "Wrapper for make to help setup/execute standard destroy/setup/run/watch commands"
   homepage "https://github.com/Flare576/vroom"
-  url "https://github.com/Flare576/vroom/archive/refs/tags/v12.0.0.tar.gz"
-  sha256 "ed80f2951f707aac75937be932d436efacefe14b6233ed5d1441f233b5bb59ea"
+  url "https://github.com/Flare576/vroom/archive/refs/tags/v12.0.1.tar.gz"
+  sha256 "6035f975af14a48e2fd75d69003e54d35b8a9f20ee9c627d8d01921afee4d5e2"
   license "MIT"
 
   depends_on "make"
