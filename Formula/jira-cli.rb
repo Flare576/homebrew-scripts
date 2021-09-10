5f3f1b85da8d50a76423f381e1836e13cc7a5181
flare576 <flare576@gmail.com>
2021-09-10 16:10:13 -0500

    bump(jira-cli)

--- Formula/jira-cli.rb
+++ Formula/jira-cli.rb
@@ -1,8 +1,8 @@
 class JiraCli < Formula
-  desc "Go-Jira/jira with Flare customizations."
+  desc "Go-Jira/jira with Flare customizations. Non-semver version: setup.cookie.config"
   homepage "https://github.com/Flare576/jira-cli"
-  url "https://github.com/Flare576/jira-cli/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "75f61afca6c7e784b9e085b63df3de741a4babbbeede63b40ce3158fbc36eb6d"
+  url "https://github.com/Flare576/jira-cli/archive/refs/tags/v3.3.8.tar.gz"
+  sha256 "3982ab52d9f0bae6bbde3f8d7c7410d232f30a1ec1e3b869a7fb397811dcc248"
   license "MIT"
 
   depends_on "node"
@@ -10,9 +10,9 @@ class JiraCli < Formula
 
   def install
     bin.install "bin/jiracookie"
-    bin.install "bin/jira-install"
-    prefix.install ".jira.d"
-    ohai "Be sure to run 'jira-install' to link .jira.d!"
+    bin.install "bin/jira-setup"
+    lib.install ".jira.d"
+    ohai "Be sure to run 'jira-setup' to link .jira.d!"
   end
 
   test do
