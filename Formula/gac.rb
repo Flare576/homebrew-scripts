fdbf07947d10f29b96b5e43003b4dc1906aa6ff3
flare576 <flare576@gmail.com>
2021-09-10 16:33:16 -0500

    bump(gac)

--- Formula/gac.rb
+++ Formula/gac.rb
@@ -1,8 +1,8 @@
 class Gac < Formula
   desc "Git Add Commit makes common git tasks easier"
   homepage "https://github.com/Flare576/gac"
-  url "https://github.com/Flare576/gac/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "69b737262772788f5e46eefac6a02f06e455d76fe2f9ea67b898243d20aca28c"
+  url "https://github.com/Flare576/gac/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "d0c430e958e1cac7adce05c10d781d63ca4843aec654eaf65b3e223686110d5b"
   license "MIT"
 
   depends_on "git"
