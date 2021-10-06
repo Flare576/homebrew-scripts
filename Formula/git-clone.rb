153e8a0215edaccca1fdf2b462d6fd70c00c85e9
flare576 <flare576@gmail.com>
2021-10-06 09:44:14 -0500

    bump(git-clone)

--- Formula/git-clone.rb
+++ Formula/git-clone.rb
@@ -1,8 +1,8 @@
 class GitClone < Formula
   desc "Tool for maintaining multiple git accounts across any git system supporting HTTPS."
   homepage "https://github.com/Flare576/git-clone"
-  url "https://github.com/Flare576/git-clone/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "b40012eff2e13deaeb1032b9c79cd8eb646a757dd113b24db3f1205762e787b8"
+  url "https://github.com/Flare576/git-clone/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "de5cc4be237894b7673726e2a4f240db91f875c9d6d4ed22f11b2481627c9377"
   license "MIT"
 
   depends_on "git"
