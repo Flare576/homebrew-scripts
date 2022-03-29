a84d548d300768f4938416f86026cb8e937d2593
flare576 <flare576@gmail.com>
2022-03-29 06:39:28 -0500

    bump(monitorjobs)

--- Formula/monitorjobs.rb
+++ Formula/monitorjobs.rb
@@ -1,8 +1,8 @@
 class Monitorjobs < Formula
   desc ""
   homepage "https://github.com/Flare576/monitorjobs"
-  url "https://github.com/Flare576/monitorjobs/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "b47f5f41e55120bb1bff194f5a262fb66f2339991c9a13978d51ca7be50640d9"
+  url "https://github.com/Flare576/monitorjobs/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "a9b0d4a404dbce24874cdd27c970818ebb7748ab1d7a93acbb7f4533213f8d5f"
   license "MIT"
 
   depends_on "jq"
