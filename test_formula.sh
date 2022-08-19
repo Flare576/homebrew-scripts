9146353bbfd82c0aec4f4b3e678cbd8959278716
flare576 <flare576@gmail.com>
2022-08-19 13:50:02 -0500

    feat(test): Add help/guardrails for formula tester

--- test_formula.sh
+++ test_formula.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+usage="Tests the formulas locally to get the hash before pushing to github.
+
+-h  Print this
+-l  Install local files. By default assumes project is in ../\$formula, otherwise pass a second param with full path
+-r  Pass 'reinstall' instead of 'install'"
+
+
 while getopts ':hrl' option; do
   case "$option" in
     h) echo "$usage"
@@ -14,6 +21,7 @@ done
 shift $((OPTIND -1))
 
 formula=./Formula/$1.rb
+[ -z "$formula" ] && echo "You need to pass a Formula" && exit
 
 if [ -n "$local_files" ]; then
   [ -z "$2" ] && source="../$1" || source="$2"
