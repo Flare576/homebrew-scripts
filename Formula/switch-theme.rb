edab7c4652c17aaf846f7469a6944db06761140d
flare576 <flare576@gmail.com>
2022-08-19 13:58:59 -0500

    bump(switch-theme)

--- Formula/switch-theme.rb
+++ Formula/switch-theme.rb
@@ -1,8 +1,8 @@
 class SwitchTheme < Formula
   desc "Tool for switching themes in vim, tmux, zsh, bat, vsCode, terminals (gnome, mintty, Terminal.app), and whatever else I figure out."
   homepage "https://github.com/Flare576/switch-theme"
-  url "https://github.com/Flare576/switch-theme/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "66a9a7faef9e789b3e142af68d5d080945e3776c12a89ac404cbe6460fd08d90"
+  url "https://github.com/Flare576/switch-theme/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "37223d7215545a1afe8c884bb69c84813aa756d6a2966b9dc640ec5f8d689350"
   license "MIT"
 
   def install
