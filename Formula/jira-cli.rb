ea2725609548f0a545cd76dfe436e55b921f9d7e
flare576 <flare576@gmail.com>
2022-09-26 12:08:53 -0500

    bump(jira-cli)

--- Formula/jira-cli.rb
+++ Formula/jira-cli.rb
@@ -1,8 +1,8 @@
 class JiraCli < Formula
   desc "Go-Jira/jira with Flare customizations. Non-semver version: setup.cookie.config"
   homepage "https://github.com/Flare576/jira-cli"
-  url "https://github.com/Flare576/jira-cli/archive/refs/tags/v3.3.8.tar.gz"
-  sha256 "3982ab52d9f0bae6bbde3f8d7c7410d232f30a1ec1e3b869a7fb397811dcc248"
+  url "https://github.com/Flare576/jira-cli/archive/refs/tags/v3.4.8.tar.gz"
+  sha256 "63679f33d8ad102432b58920fe160b166868da757770ee9704227de343778c9e"
   license "MIT"
 
   depends_on "node"
