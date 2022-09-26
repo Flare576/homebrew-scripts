220500278f35c348e958c1fc658943d523fd74c7
flare576 <flare576@gmail.com>
2022-09-26 11:56:38 -0500

    bump(newScript)

--- Formula/newScript.rb
+++ Formula/newScript.rb
@@ -1,8 +1,8 @@
 class Newscript < Formula
   desc "Quickly generate script stubs in js, sh, or py"
   homepage "https://github.com/Flare576/newScript"
-  url "https://github.com/Flare576/newScript/archive/refs/tags/v0.0.7.tar.gz"
-  sha256 "c167ab9a7b399858a17242d0f03c558e6bbef2d521655db8e2cf867e0c3a8b5c"
+  url "https://github.com/Flare576/newScript/archive/refs/tags/v0.0.8.tar.gz"
+  sha256 "d6a9127decbaa1d1eaaf7b1335dafb989180f741ad092285cf6b00feb5dcb58a"
   license "MIT"
 
   depends_on "node"
