767a7e5e771ceb821a8ff4301a8b1b83d1b1725e
flare576 <flare576@gmail.com>
2026-05-23 16:09:56 -0500

    bump(switch-theme): v2.3.2 - kitty native theme support

--- Formula/switch-theme.rb
+++ Formula/switch-theme.rb
@@ -1,8 +1,8 @@
 class SwitchTheme < Formula
   desc "Tool for switching themes in vim, tmux, zsh, bat, vsCode, Azure Data Studio, terminals (gnome, mintty, Terminal.app), and whatever else I figure out. Be sure to run 'switch-theme -s' after install"
   homepage "https://github.com/Flare576/switch-theme"
-  url "https://github.com/Flare576/switch-theme/archive/refs/tags/v2.3.1.tar.gz"
-  sha256 "58205f6a394cf3d33c2fe2fbfb0bac096e33914b48a31db28169e44045cfdf93"
+  url "https://github.com/Flare576/switch-theme/archive/refs/tags/v2.3.2.tar.gz"
+  sha256 "b38dcadfef46f419550fc6b72eb4f53a74148b955c07f45c42d22740e6986b85"
   license "MIT"
 
   def install
